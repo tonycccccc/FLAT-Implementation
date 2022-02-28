196e7f6238c80102ebecb7c04baa713eae9f4b76
tonycccccc <chenzeyu01@hotmail.com>
2022-02-28 13:37:17 -0500

    Update: script file

--- cpu_script.sh
+++ cpu_script.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
 idx=1
+#How many times to repeat this test
 for i in {1..2}
 do
     start=256
     end=16384
-    batch=4
-    for j in {1..1}
+    head=1
+    #Increase number from 1 to 16
+    for j in {1..16}
     do
-        printf "batch number: %d" $batch
+        printf "head number: %d" $head
         curr_dt=$(date "+%Y.%m.%d-%H.%M.%S")
-        path='./batchdata/'$curr_dt
+        path='./data/'$curr_dt
         python3 ./Testing.py \
-        --size=$batch \
-        --mode="batch"\
+        --size=$head \
+        --mode="head"\
         --Start_size=$start\
         --End_size=$end \
         --Loop_NUM=$i \
         --Store_path=$path \
-        --Test="customized" \
+        --Test="length" \
         --Running_platform="CPU"
         if test -f "memoryfile.txt"; then
             echo "memoryfile.txt exists."
             mv "memoryfile.txt" $HOME"/flat/batchdata/"$curr_dt
         fi
-        cd ./batchdata
-        new_name="file-$idx-256-16k-batchtest"
+        cd ./data
+        new_name="file-$idx-256-16k-headtest"
         mv $curr_dt $new_name
         idx=$((idx+1))
-        #head=$((head+1))
+        head=$((head+1))
         cd ..
     done
 done
